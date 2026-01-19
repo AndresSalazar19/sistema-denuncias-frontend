155a3c17930f11530a5d31270cd201e39ed319c7
AndresSalazar19 <aasalazar@espol.edu.ec>
2026-01-18 21:20:19 -0500

    feature: consulta

--- src/views/Consulta.jsx
+++ src/views/Consulta.jsx
@@ -30,7 +30,7 @@ export default function Consulta() {
 
     try {
       const response = await fetch(
-        `http://127.0.0.1:8081/api/denuncias/consultar/${searchCode.trim()}`
+        `http://127.0.0.1:8000/api/denuncias/consultar/${searchCode.trim()}`
       );
 
       if (response.ok) {
